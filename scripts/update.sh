801f1e3f05b9003b92d674a6e0003cba966c6761
Lukas Kaupp <lukas.kaupp@h-da.de>
2021-05-20 13:03:53 +0200

    initial commit with first collected incomplete data. - Testing Phase

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,9 @@
 #modified version https://stackoverflow.com/a/36337403/4779586, Márcio Souza Júnior
 @ECHO OFF
 SET /p comment=DailyCommit
+git stash
 git pull
+git stash pop
 git add *
 git commit -a -m "%comment%"
 git push
